29ed02d3e89d8c7e8479c6ee9940bda92e17ac1c
queryops <gallilloss@gmail.com>
2026-03-26 23:42:23 -0500

    Deploy to GitHub Pages

---
+++ _next/static/5NIhI37ikHfCS4oVFfQ9V/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Casa_Sol//_next/:path+",
+        "destination": "/Casa_Sol/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
